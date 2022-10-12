da11adc4a488e968afd1f399a93798c0f011a6d9
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2022-10-12 12:55:41 +0300

    db + response

--- server/server/app.js
+++ server/server/app.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require(`./schema/schema`)
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { schema } from './schema/schema';
+import connect from 'mongoose';
+import connection from 'mongoose';
 
 const app = express();
 const PORT = 3005;
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql:true,
-}));
+connect(`mongodb+srv://ivan:ivan@cluster0.gawvhz2.mongodb.net/?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
-app.listen(PORT, err => {
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+const dbConnection = connection;
+dbConnection.on('error', (err) => console.log(`Connection error:${err} `));
+dbConnection.once('open', () => console.log('connected'));
+
+app.listen(PORT, (err) => {
   err ? console.log(err) : console.log('Server gamno!');
 });
--- server/server/schema/schema.js
+++ server/server/schema/schema.js
@@ -1,23 +1,61 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } =
+  graphql;
 
+/*
+// All IDs set automatically by mLab
+// Don't forget to update after creation
+const directorsJson = [
+  { "name": "Quentin Tarantino", "age": 55 }, // 63458d8269101c547e0f3c74
+  { "name": "Michael Radford", "age": 72 }, // 63458ddb69101c547e0f3c75
+  { "name": "James McTeigue", "age": 51 }, // 63458e0169101c547e0f3c76
+  { "name": "Guy Ritchie", "age": 50 }, // 63458e1d69101c547e0f3c77
+];
+// directorId - it is ID from the directors collection
+const moviesJson = [
+  { "name": "Pulp Fiction", "genre": "Crime", "directorId": "63458d8269101c547e0f3c74" },
+  { "name": "1984", "genre": "Sci-Fi", "directorId": "63458ddb69101c547e0f3c75" },
+  { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "63458e0169101c547e0f3c76" },
+  { "name": "Snatch", "genre": "Crime-Comedy", "directorId": "63458e1d69101c547e0f3c77" },
+  { "name": "Reservoir Dogs", "genre": "Crime", "directorId": "63458d8269101c547e0f3c74" },
+  { "name": "The Hateful Eight", "genre": "Crime", "directorId": "63458d8269101c547e0f3c74" },
+  { "name": "Inglourious Basterds", "genre": "Crime", "directorId": "63458d8269101c547e0f3c74" },
+  { "name": "Lock, Stock and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": "63458e1d69101c547e0f3c77" },
+];
 const movies = [
-	{ id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
-	{ id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
-	{ id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
-	{ id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
-	{ id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
-	{ id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
-	{ id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
-	{ id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
+  { id: '1', name: "Pulp Fiction", genre: "Crime", directorId: "1" },
+  { id: '2', name: "1984", genre: "Sci-Fi", directorId: "2" },
+  { id: '3', name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: '4', name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: '5', name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
+  { id: '6', name: "The Hateful Eight", genre: "Crime", directorId: "1" },
+  { id: '7', name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  { id: '8', name: "Lock, Stock and Two Smoking Barrels", genre: "Crime-Comedy", directorId: "4" },
+];
+const directors = [
+	{ id: '1', name: "Quentin Tarantino", age: 55 },
+  { id: '2', name: "Michael Radford", age: 72 },
+  { id: '3', name: "James McTeigue", age: 51 },
+  { id: '4', name: "Guy Ritchie", age: 50 },
+];
+*/
+const movies = [
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
+  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
+  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3' },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4' },
+  { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
+  { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
+  { id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
+  { id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' }
 ];
 
 const directors = [
-	{ id: '1', name: 'Quentin Tarantino', age: 55 },
-	{ id: '2', name: 'Michael Radford', age: 72 },
-	{ id: '3', name: 'James McTeigue', age: 51 },
-	{ id: '4', name: 'Guy Ritchie', age: 50 },
+  { id: '1', name: 'Quentin Tarantino', age: 55 },
+  { id: '2', name: 'Michael Radford', age: 72 },
+  { id: '3', name: 'James McTeigue', age: 51 },
+  { id: '4', name: 'Guy Ritchie', age: 50 }
 ];
 
 const MovieType = new GraphQLObjectType({
@@ -26,13 +64,13 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
-		director: {
-			type: DirectorType,
-			resolve(parent, args) {
-				return directors.find(director => director.id === parent.id);
-			}
-		}
-  }),
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find((director) => director.id === parent.id);
+      }
+    }
+  })
 });
 
 const DirectorType = new GraphQLObjectType({
@@ -41,13 +79,13 @@ const DirectorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
-		movies: {
-			type: new GraphQLList(MovieType),
-			resolve(parent, args) {
-				return movies.filter(movie => movie.directorId === parent.id);
-			},
-		},
-  }),
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter((movie) => movie.directorId === parent.id);
+      }
+    }
+  })
 });
 
 const Query = new GraphQLObjectType({
@@ -57,31 +95,31 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find(movie => movie.id === args.id);
-      },
+        return movies.find((movie) => movie.id === args.id);
+      }
     },
-		director: {
+    director: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return directors.find(director => director.id === args.id);
-      },
+        return directors.find((director) => director.id === args.id);
+      }
     },
-		movies: {
-			type: new GraphQLList(MovieType),
-			resolve(parent, args) {
-				return movies;
-			}
-		},
-		directors: {
-			type: new GraphQLList(DirectorType),
-			resolve(parent, args) {
-				return directors;
-			}
-		}
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies;
+      }
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      resolve(parent, args) {
+        return directors;
+      }
+    }
   }
 });
 
 module.exports = new GraphQLSchema({
-  query: Query,
+  query: Query
 });
